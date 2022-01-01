bac119cb77284e397a81c27ee1b6b6f011ac1e18
Justin Garner <j.garner828@gmail.com>
2022-01-01 17:46:41 -0500

    started index.js framework

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+  
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
